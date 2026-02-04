2eb5aa173ff2b589e12d98cda93a8a6909efe34f
Rahul0725 <60087338+Rahul0725@users.noreply.github.com>
2026-02-04 23:44:20 +0530

    feat: Add second game over sound constant
    Introduces a new constant for a secondary game over sound effect, enabling more variety in game-ending audio.

--- constants.ts
+++ constants.ts
@@ -27,4 +27,4 @@ export const BACKGROUND_MUSIC_URL = 'https://github.com/Rahul0725/Sound-effects-
 export const GAME_OVER_SOUND_URL = 'https://github.com/Rahul0725/Sound-effects-gg/raw/refs/heads/main/92116289-98e0-4dea-babc-e6f7f8bd08d9.mp3';
 
 // Game Over Sound URL 2 (Second Attempt)
-export const GAME_OVER_SOUND
+export const GAME_OVER_SOUND_URL_2 = 'https://github.com/Rahul0725/Sound-effects-/raw/refs/heads/main/%E0%A6%86%E0%A6%B8%E0%A6%B8%E0%A6%BE%E0%A6%B2%E0%A6%BE%E0%A6%AE%E0%A7%81%20%E0%A6%86%E0%A6%B2%E0%A6%BE%E0%A6%87%E0%A6%95%E0%A7%81%E0%A6%AE%20%E0%A6%AC%E0%A6%A1%E0%A6%BC%20%E0%A6%AD%E0%A6%BE%E0%A6%87%20%E0%A5%A4%E0%A5%A4%20normal%20screen%20%E0%A5%A4%E0%A5%A4%20dipjol%20dialogue(MP3_160K).mp3';
